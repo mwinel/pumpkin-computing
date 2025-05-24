d677a9fb5e586866866bb321476f0e246f873d76
Nelson M <nelsonmwirumubi@gmail.com>
2025-05-24 11:34:33 +0300

    remove unused component

--- src/components/mobile-menu.tsx
+++ src/components/mobile-menu.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Menu, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
